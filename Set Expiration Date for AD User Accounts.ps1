17ff4837bb8863f384c4e3463cbc99b4cc3ae4ec
Nathan Alston <alston.nathanj@gmail.com>
2021-09-10 19:22:54 -0400

    Update Set Expiration Date for AD User Accounts.ps1

--- Set Expiration Date for AD User Accounts.ps1
+++ Set Expiration Date for AD User Accounts.ps1
@@ -2,6 +2,6 @@ $date = Get-Date
 $users = Get-ADUser -Filter {GivenName -like "A*"} -Properties AccountExpirationDate
 
 ForEach ($user in $users)
-  {
-  Set-ADUser $user -AccountExpirationDate $date.AddHours(-20)
-  }
+{
+Set-ADUser $user -AccountExpirationDate $date.AddHours(-20)
+}
